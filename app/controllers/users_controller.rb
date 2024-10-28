8fb712fd644007f16a6a0db6c9b9e13bfbec8ac9
Ashanti Hatchett <ahatchett02@gmail.com>
2024-10-28 17:27:42 +0000

    fixing UsersController error (not displaying users#feed) - in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,45 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
 
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  #####
+
+  def show
+  end
+
+  def liked
+  end
+
+  def feed
+    authorize @user
+  end
+
+  def followers
+  end
+
+  def following
+  end
+
+  def discover
+  end
+
   private
 
     def set_user
